c73492176c1f5406d02245e215a56135781e5c1d
SamplesMaker <insert@kakao.com>
2020-02-28 00:00:13 +0900

    Swift UI MapView Sample

--- MapKitSample/ContentView.swift
+++ MapKitSample/ContentView.swift
@@ -7,10 +7,38 @@
 //
 
 import SwiftUI
+import MapKit
+
+
+struct MapView: UIViewRepresentable {
+    
+    func makeUIView(context: UIViewRepresentableContext<MapView>) -> MKMapView {
+        MKMapView(frame: .zero)
+    }
+    
+    func updateUIView(_ uiView: MKMapView, context: UIViewRepresentableContext<MapView>) {
+        let location = CLLocationCoordinate2D(latitude: 51.50007773, longitude: -0.1246402)
+        
+        let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+        let region = MKCoordinateRegion(center: location, span: span)
+        
+        uiView.setRegion(region, animated: true)
+        
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = location
+        annotation.title = "Big Ben"
+        annotation.subtitle = "London"
+        uiView.addAnnotation(annotation)
+        
+    }    
+}
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        NavigationView {
+            MapView()
+            .navigationBarTitle(Text("MapView"))
+        }
     }
 }
 
